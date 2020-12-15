e593148e0804957221edc069facdcf33a36fe8e4
Save-1401 <75083914+Save-1401@users.noreply.github.com>
2020-12-15 09:59:48 -0600

    Someone to talk to created

---
+++ Day Three_Someone To Talk To/Day Three/Day Three/ConversationPhrase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_Three
+{
+    class ConversationPhrase
+    {
+        /// <summary>
+        /// The phrase that is said if this conversation phrase can be said
+        /// </summary>
+        public string phrase;
+        public string[] wordsThatMustBeSaid;
+        public bool or;
+
+        public ConversationPhrase(string phrase, string[] wordsThatMustBeSaid)
+        {
+            this.phrase = phrase;
+            this.wordsThatMustBeSaid = wordsThatMustBeSaid;
+            or = false;
+        }
+        public ConversationPhrase(string phrase, string[] wordsThatMustBeSaid, bool or)
+        {
+            this.phrase = phrase;
+            this.wordsThatMustBeSaid = wordsThatMustBeSaid;
+            this.or = or;
+        }
+        /// <summary>
+        /// Checks if the conversation phrase can be said
+        /// </summary>
+        /// <returns>If the conversation phrase can be said</returns>
+        public bool CheckIfCanBeSaid(string userInput)
+        {
+            //If the phrase can be said
+            bool canBeSaid = true;
+            //Checks each word that must be said to see if it is in the player input
+            foreach (string word in wordsThatMustBeSaid)
+            {
+                if (or)
+                    canBeSaid = canBeSaid || Program.Contains(userInput.ToLower(), word.ToLower());
+                else
+                    canBeSaid = canBeSaid && Program.Contains(userInput.ToLower(), word.ToLower());
+            }
+
+            //Returns if the phrase can be said
+            return canBeSaid;
+        }
+    }
+}
---
+++ Day Three_Someone To Talk To/Day Three/Day Three/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_Three
+{
+    class Program
+    {
+        /// <summary>
+        /// All the things the program can say
+        /// </summary>
+        static ConversationPhrase[] thingsToSay = new ConversationPhrase[] { new ConversationPhrase("My name is Someone To Talk To", new string[] { "What", "Your", "Name"}), 
+            new ConversationPhrase("That is insulting", new string[] { "You", "Stupid" }), new ConversationPhrase("Yes, user?", new string[] { "Someone To Talk To"}), 
+            new ConversationPhrase("GOOD", new string[] { "Good" }), new ConversationPhrase("GOOD!", new string[] { "I hate you" }), new ConversationPhrase("Talk to you later", new string[] { "Goodbye", "Cya", "See you", "See you later" }, true),
+            new ConversationPhrase("Goodbye", new string[] { "Goodbye", "Cya", "See you", "See you later" }, true), new ConversationPhrase("It was nice talking to you", new string[] { "Goodbye", "Cya", "See you", "See you later" }, true) };
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(1401);
+
+            while (true)
+            {
+                string toSay = Speak(Console.ReadLine(), rnd);
+                Console.WriteLine(toSay);
+
+                if (toSay == "Talk to you later" || toSay == "Goodbye" || toSay == "It was nice talking to you")
+                {
+                    return;
+                }
+            }
+        }
+        /// <summary>
+        /// Checks the user input and decides on something to say based off it
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns>The thing decided to say</returns>
+        static string Speak(string userInput, Random rnd)
+        {
+            //What to say
+            string toSay = "";
+            //List of conversation phrases that can be said
+            List<ConversationPhrase> thingsThatCanBeSaid = new List<ConversationPhrase>();
+
+            //Gets all the phrases that can be said
+            foreach (ConversationPhrase cp in thingsToSay)
+            {
+                if (cp.CheckIfCanBeSaid(userInput))
+                {
+                    thingsThatCanBeSaid.Add(cp);
+                }
+            }
+
+            //Chooses a random thing that can be said's phrase to say, if there are no things that can be said, tells the user that it does not understand what they said
+            if (thingsThatCanBeSaid.Count > 0)
+                toSay = thingsThatCanBeSaid[rnd.Next(0, thingsThatCanBeSaid.Count)].phrase;
+            else
+                toSay = "I'm sorry, I do not understand";
+
+            //Returns what has been decided to say
+            return toSay;
+        }
+        /// <summary>
+        /// Checks if container contains word
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="word"></param>
+        /// <returns>Whether or not container contains word</returns>
+        public static bool Contains(string container, string word)
+        {
+            //If word is contained in container
+            bool contains = false;
+            //The letter of word currently being checked
+            int letter = 0;
+
+            //Iterates through container and checks if word is contained in it
+            char[] containerCA = container.ToCharArray();
+            for (int i = 0; i < containerCA.Length; i++)
+            {
+                if (containerCA[i] == word.ToCharArray()[letter])
+                {
+                    letter++;
+                    if (letter >= word.Length)
+                    {
+                        contains = true;
+                        letter = 0;
+                    }
+                }
+                else
+                {
+                    letter = 0;
+                }
+            }
+
+            //Returns the results
+            return contains;
+        }
+    }
+}
