08ae5c29acc406aeebbca26b44ee6e4e753e6795
Save-1401 <75083914+Save-1401@users.noreply.github.com>
2020-12-23 10:20:03 -0600

    Created day ten

---
+++ Day Ten_Dice Roller/Day Ten/Day Ten/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Day_Ten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sets the random
+            Random rnd;
+            Console.Write("Enter a seed: ");
+            try
+            {
+                rnd = new Random(Convert.ToInt32(Console.ReadLine()));
+            }
+            catch
+            {
+                rnd = new Random(1401);
+            }
+
+            //The number of sides last used
+            int lastSides = 1;
+            //Infinitely loops
+            while (true)
+            {
+                //Sets the sides that the dice has
+                int sides = 1;
+                Console.Write("Enter dice sides: ");
+                try
+                {
+                    sides = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    //If the input is invalid, sets the dices sides to the last sides
+                    sides = lastSides;
+                }
+
+                //Rolls the dice
+                int results = rnd.Next(1, sides + 1);
+
+                //Prints the results
+                Console.WriteLine(results);
+
+                //Adds a space
+                Console.WriteLine("");
+
+                //Sets the last sides the the sides
+                lastSides = sides;
+            }
+        }
+    }
+}
