7a359fbaf5f74cb61863f79a9024ae197443f9ac
Save-1401 <75083914+Save-1401@users.noreply.github.com>
2020-12-24 09:45:19 -0600

    Day Eleven Created

---
+++ Day Eleven_Count Forever/Day Eleven/Day Eleven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_Eleven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The number being printed
+            int i = 0;
+            //Endlessly writes the number being printed and then adds one to it
+            while (true)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
