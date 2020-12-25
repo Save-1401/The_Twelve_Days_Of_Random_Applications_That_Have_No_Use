d13debea44c982f27020df3e3ec7a82d01369423
Save-1401 <75083914+Save-1401@users.noreply.github.com>
2020-12-25 09:36:10 -0600

    Day Twelve Created

---
+++ Day Twelve_Merry Cristmas/Day Twelve/Day Twelve/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day_Twelve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merry Christmas!");
+            Console.ReadLine();
+        }
+    }
+}
